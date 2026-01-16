a1b271f0e8b3592881f6bb63bbc309726c4fe658
Erick <espinozae545@gmail.com>
2026-01-16 12:34:27 -0400

    primer despliegue

--- app/model_loader.py
+++ app/model_loader.py
@@ -6,6 +6,4 @@ model = tf.keras.models.load_model("app/modeloOptimo.h5")
 # Mostrar resumen completo
 model.summary()
 
-# O listar solo los nombres de las capas
-for i, layer in enumerate(model.layers):
-    print(i, layer.name, layer.output_shape)
+
